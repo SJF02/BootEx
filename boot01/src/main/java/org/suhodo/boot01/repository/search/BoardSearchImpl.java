cc912cc7faa9f427275657be0337d0eef5e644f2
SJF02 <suhodo1126@gmail.com>
2025-06-02 17:19:22 +0900

    QueryDsl로 페이징 처리 구현함

--- boot01/src/main/java/org/suhodo/boot01/repository/search/BoardSearchImpl.java
+++ boot01/src/main/java/org/suhodo/boot01/repository/search/BoardSearchImpl.java
@@ -3,12 +3,13 @@ package org.suhodo.boot01.repository.search;
 import java.util.List;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
-import org.springframework.data.querydsl.QuerydslRepositoryInvokerAdapter;
 import org.suhodo.boot01.domain.Board;
 import org.suhodo.boot01.domain.QBoard;
 
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.jpa.JPQLQuery;
 
 /*
@@ -47,7 +48,42 @@ public class BoardSearchImpl extends QuerydslRepositorySupport implements BoardS
     public Page<Board> searchAll(String[] types, String keyword, Pageable pageable) {
 
         QBoard board = QBoard.board;
+        JPQLQuery<Board> query = from(board);       // FROM board
 
-        return null;
+        if((types!=null && types.length>0) && keyword!=null){
+            BooleanBuilder booleanBuilder = new BooleanBuilder();
+
+            for(String type: types){
+                switch(type){
+                    case "t":
+                        booleanBuilder.or(board.title.contains(keyword));
+                        break;
+                    case "c":
+                        booleanBuilder.or(board.content.contains(keyword));
+                        break;
+                    case "w":
+                        booleanBuilder.or(board.writer.contains(keyword));
+                        break;
+                }
+            }
+            /*
+             * WHERE title LIKE '%{keyword}%'
+             *    OR content LIKE '%{keyword}'
+             *    OR writer LIKE '%{keyword}'
+             */
+            
+            query.where(booleanBuilder);
+        }
+
+        // WHERE bno > 0
+        query.where(board.bno.gt(0L));
+
+        this.getQuerydsl().applyPagination(pageable, query);
+
+        List<Board> list = query.fetch();
+
+        long count = query.fetchCount();
+
+        return new PageImpl<>(list, pageable, count);
     }    
 }
