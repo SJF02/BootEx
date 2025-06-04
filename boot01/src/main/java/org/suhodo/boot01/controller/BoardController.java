769f7546209a13c3f9ea40d84c3e69ecb5d3b3c4
SJF02 <suhodo1126@gmail.com>
2025-06-04 17:01:04 +0900

    Post 요청으로 새로운 데이터 등록 메서드 추가중...

--- boot01/src/main/java/org/suhodo/boot01/controller/BoardController.java
+++ boot01/src/main/java/org/suhodo/boot01/controller/BoardController.java
@@ -2,13 +2,17 @@ package org.suhodo.boot01.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.suhodo.boot01.dto.BoardDTO;
 import org.suhodo.boot01.dto.PageRequestDTO;
 import org.suhodo.boot01.dto.PageResponseDTO;
 import org.suhodo.boot01.service.BoardService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
@@ -28,5 +32,23 @@ public class BoardController {
         log.info(responseDTO);
 
         model.addAttribute("responseDTO", responseDTO);
+    }
+
+    @GetMapping("/register")
+    public void registerGET(){
+
+    }
+
+    @PostMapping("/register")
+    public String registerPost(@Valid BoardDTO boardDTO, BindingResult bindingResult,
+                            RedirectAttributes redirectAttributes){
+    
+        log.info("board POST register...........");
+
+        if(bindingResult.hasErrors()){
+            log.info("has errors...........");
+            redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
+            return "redirect:/board/register";
+        }
     }
 }
