c4993f9c5357dc2b5f882d3b03aef97dd81e7e39
SJF02 <suhodo1126@gmail.com>
2025-06-05 13:11:20 +0900

    ReplyController의 register에 @Valid 추가

--- boot01/src/main/java/org/suhodo/boot01/controller/ReplyController.java
+++ boot01/src/main/java/org/suhodo/boot01/controller/ReplyController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.suhodo.boot01.dto.ReplyDTO;
 
 import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
 import lombok.extern.log4j.Log4j2;
 
 @RestController
@@ -22,7 +23,7 @@ public class ReplyController {
     
     @Operation(description="Replies POST")
     @PostMapping(value="/", consumes=MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Map<String, Long>> register(@RequestBody ReplyDTO replyDTO){
+    public ResponseEntity<Map<String, Long>> register(@Valid @RequestBody ReplyDTO replyDTO){
 
         log.info(replyDTO);
 
