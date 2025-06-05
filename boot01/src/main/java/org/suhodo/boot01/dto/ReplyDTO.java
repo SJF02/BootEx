79f030894b8de8751ae86f099b8b64d78d977379
SJF02 <suhodo1126@gmail.com>
2025-06-05 13:07:28 +0900

    ReplyDTO에 검증 조건 추가

--- boot01/src/main/java/org/suhodo/boot01/dto/ReplyDTO.java
+++ boot01/src/main/java/org/suhodo/boot01/dto/ReplyDTO.java
@@ -2,6 +2,8 @@ package org.suhodo.boot01.dto;
 
 import java.time.LocalDateTime;
 
+import groovyjarjarantlr4.v4.runtime.misc.NotNull;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,9 +14,17 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ReplyDTO {
+
     private Long rno;
+
+    @NotNull
     private Long bno;
+
+    @NotEmpty
     private String replyText;
+
+    @NotEmpty
     private String replyer;
+    
     private LocalDateTime regDate, modDate;
 }
