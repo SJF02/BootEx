4afecf062727e065e79147129640e26c5b34a37c
SJF02 <suhodo1126@gmail.com>
2025-06-05 15:30:41 +0900

    list.html에 replyCount 적용

--- boot01/src/main/java/org/suhodo/boot01/controller/BoardController.java
+++ boot01/src/main/java/org/suhodo/boot01/controller/BoardController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.suhodo.boot01.dto.BoardDTO;
+import org.suhodo.boot01.dto.BoardListReplyCountDTO;
 import org.suhodo.boot01.dto.PageRequestDTO;
 import org.suhodo.boot01.dto.PageResponseDTO;
 import org.suhodo.boot01.service.BoardService;
@@ -28,7 +29,9 @@ public class BoardController {
     @GetMapping("/list")
     public void list(PageRequestDTO pageRequestDTO, Model model){
         
-        PageResponseDTO<BoardDTO> responseDTO = boardService.list(pageRequestDTO);
+        // PageResponseDTO<BoardDTO> responseDTO = boardService.list(pageRequestDTO);
+        PageResponseDTO<BoardListReplyCountDTO> responseDTO = 
+            boardService.listWithReplyCount(pageRequestDTO);
 
         log.info(responseDTO);
 
