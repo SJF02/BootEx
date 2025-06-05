1ecbabd8dae354a96b0d574e76e7e48339eeb85a
SJF02 <suhodo1126@gmail.com>
2025-06-05 14:45:13 +0900

    게시판 글에 댓글 갯수를 표시하기 위해 BoardListReplyCountDTO를 생성함.

--- boot01/src/test/java/org/suhodo/boot01/repository/ReplyRepositoryTests.java
+++ boot01/src/test/java/org/suhodo/boot01/repository/ReplyRepositoryTests.java
@@ -3,6 +3,10 @@ package org.suhodo.boot01.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.suhodo.boot01.domain.Board;
 import org.suhodo.boot01.domain.Reply;
 
@@ -28,5 +32,18 @@ public class ReplyRepositoryTests {
                         .build();
 
         replyRepository.save(reply);
+    }
+
+    @Test
+    public void testBoardReplies(){
+        Long bno = 100L;
+
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("rno").descending());
+
+        Page<Reply> result = replyRepository.listOfBoard(bno, pageable);
+
+        result.getContent().forEach(reply -> {
+            log.info(reply);
+        });
     }
 }
