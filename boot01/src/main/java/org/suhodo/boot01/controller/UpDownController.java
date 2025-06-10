99f2a6b90b8d1e0bf93bf27f3849a87921b9984e
SJF02 <suhodo1126@gmail.com>
2025-06-10 12:56:12 +0900

    파일 업로드시 파일 이름 출력

--- boot01/src/main/java/org/suhodo/boot01/controller/UpDownController.java
+++ boot01/src/main/java/org/suhodo/boot01/controller/UpDownController.java
@@ -2,6 +2,7 @@ package org.suhodo.boot01.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,11 +16,20 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 public class UpDownController {
 
+    @Value("{org.suhodo.upload.path}")
+    private String uploadPath;
+
     @Operation(description = "POST 방식으로 파일 업로드")
     @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public List<UploadFileDTO> upload(UploadFileDTO uploadFileDTO){
         
         log.info(uploadFileDTO);
+
+        if(uploadFileDTO.getFiles() != null){
+            uploadFileDTO.getFiles().forEach(multipartFile -> {
+                log.info(multipartFile.getOriginalFilename());
+            });
+        }
 
         return null;
     }
