3aa5f0f5824279d1530d23bedec44bd1d5cc55b0
SJF02 <suhodo1126@gmail.com>
2025-06-11 11:45:17 +0900

    searchWithAll에 검색 조건 추가

--- boot01/src/main/java/org/suhodo/boot01/repository/search/BoardSearchImpl.java
+++ boot01/src/main/java/org/suhodo/boot01/repository/search/BoardSearchImpl.java
@@ -177,6 +177,26 @@ public class BoardSearchImpl extends QuerydslRepositorySupport implements BoardS
         JPQLQuery<Board> boardJPQLQuery = from(board);
         boardJPQLQuery.leftJoin(reply).on(reply.board.eq(board));
 
+        // 검색 조건 추가
+        if((types != null && types.length > 0) && keyword != null){
+            BooleanBuilder booleanBuilder = new BooleanBuilder();
+
+            for(String type : types){
+                switch(type){
+                    case "t":
+                        booleanBuilder.or(board.title.contains(keyword));
+                        break;
+                    case "c":
+                        booleanBuilder.or(board.content.contains(keyword));
+                        break;
+                    case "w":
+                        booleanBuilder.or(board.writer.contains(keyword));
+                        break;
+                }
+            }
+            boardJPQLQuery.where(booleanBuilder);
+        }
+
         boardJPQLQuery.groupBy(board);
 
         getQuerydsl().applyPagination(pageable, boardJPQLQuery);
