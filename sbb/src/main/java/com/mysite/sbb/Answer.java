b7f6ae4344db3c84ebfbb5e14ffaec46428b155e
SJF02 <suhodo1126@gmail.com>
2025-06-16 16:37:51 +0900

    Question 테이블 테스트

--- sbb/src/main/java/com/mysite/sbb/Answer.java
+++ sbb/src/main/java/com/mysite/sbb/Answer.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -23,5 +24,6 @@ public class Answer {
 
     private LocalDateTime createDate; 
 
-    // private Question question;  
+    @ManyToOne
+    private Question question;  
 }
--- sbb/src/main/java/com/mysite/sbb/Question.java
+++ sbb/src/main/java/com/mysite/sbb/Question.java
@@ -1,13 +1,16 @@
 package com.mysite.sbb;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import jakarta.annotation.Generated;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -27,4 +30,7 @@ public class Question {
     private String content;
 
     private LocalDateTime createDate;
+    
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    private List<Answer> answerList;
 }
