bae7355c946e5f52f8a842fcd7c258819bb7a8c3
SJF02 <suhodo1126@gmail.com>
2025-06-17 15:54:05 +0900

    상세페이지 기능 추가

--- sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import lombok.RequiredArgsConstructor;
 
@@ -22,5 +23,15 @@ public class QuestionController {
 
         // question_list.html로 이동하라
         return "question_list";
+    }
+
+    @GetMapping(value = "/question/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id){
+
+        Question question = this.questionService.getQuestion(id);
+        model.addAttribute("question", question);
+
+        // question_detail.html로 이동하라
+        return "question_detail";
     }
 }
